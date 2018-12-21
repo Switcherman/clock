b2b1dc8841ceea4ba957280ea38bf363488d6520
Switcherman <418211500@163.com>
2018-12-21 18:16:29 +0800

    cron表达式解析优化,增加环境变量配置可以脚本运行

--- com/pctf/clockboot.py
+++ com/pctf/clockboot.py
@@ -2,28 +2,30 @@
 import json
 import sys
 import time
-
-from apscheduler.schedulers.background import BackgroundScheduler
-
-from com.pctf.entity.clock import dict_to_clock
 import os
 
+root_path = os.path.abspath(os.path.join(os.getcwd(), "../../"))
+sys.path.append(root_path)
+default_sound = os.path.join(root_path, "default.mp3")
+config_file = os.path.join(root_path, "clockconfiguration.json")
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from com.pctf.entity.clock import dict_to_clock
 from com.pctf.event.clock_event import ring
 
 '''this is a clock program for mac
     author: lucifer
     date: 2018-12-17
 '''
-root_path = os.path.abspath(os.path.join(os.getcwd(), "../../"))
-default_sound = os.path.join(root_path, "default.mp3")
-config_file = os.path.join(root_path, "clockconfiguration.json")
+
 def boot():
     args = sys.argv
     print(args)
     clocks = load_config(config_file)
     jobs, scheduler = init_scheduler(clocks)
     scheduler.start()
-    time.sleep(6000)
+    while True:
+        time.sleep(100)
 
 # load clock config
 def load_config(file):
@@ -43,7 +45,7 @@ def init_scheduler(clocks):
         if clock.sound == 'default':
             clock.sound = default_sound
         jobs.append(scheduler.add_job(ring, 'cron', second=crons[0], minute=crons[1], hour=crons[2], day=crons[3],
-                          month=crons[4], week=crons[5], args=[clock], id=str(len(jobs)), name=name))
+                          month=crons[4], day_of_week=crons[5], args=[clock], id=str(len(jobs)), name=name))
     return jobs, scheduler
 
 if __name__ == '__main__':
